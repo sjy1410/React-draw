fd30c0d502f3f90eab390c137347014f5e04f1b9
songjuyoung-1410 <chicken010208@gmail.com>
2023-01-10 17:13:49 +0900

    graph

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import E3 from "./components/e-3";
 import E4 from "./components/e-4";
 import E5 from "./components/e-5";
 import E6 from "./components/e-6";
+import E7 from "./components/e-7";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
       {/*<E3></E3>*/}
       {/*<E4></E4>*/}
       {/*<E5></E5>*/}
-      {<E6></E6>}
+      {/*<E6></E6>*/}
+      {<E7></E7>}
     </div>
   );
 }
